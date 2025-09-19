21462528326ec2224569fb51f91471a2df452af4
darrenak403 <ngothanhdat4002@gmail.com>
2025-09-19 15:12:44 +0700

    Add refresh token endpoint and related logic
    Introduces a new endpoint for refreshing user tokens in `AuthV1Endpoints.cs` with the `RefreshTokenAsync` method. Adds `UserRefreshTokenDto` for data transfer and implements the `RefreshTokenUser` request and `RefreshTokenUserHandler` for processing. Removes the commented-out `GenerateAndMapRefreshToken` method in `ITokenService.cs` to simplify token generation logic. Updates `TokenService.cs` with necessary using directives. These changes enhance the authentication mechanism for improved user experience.

---
+++ EV-Station.Application/Users/DTOs/Requests/UserRefreshTokenDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EV_Station.Application.Users.DTOs.Requests
+{
+    public record UserRefreshTokenDto
+    (
+        Guid userId,
+        string refreshToken
+    );
+}
---
+++ EV-Station.Application/Users/Querries/RefreshTokenUser.cs
@@ -0,0 +1,11 @@
+﻿using EV_Station.Application.Common.Responses;
+using EV_Station.Application.Users.DTOs.Requests;
+using EV_Station.Application.Users.DTOs.Response;
+using MediatR;
+
+namespace EV_Station.Application.Users.Querries
+{
+
+    public record RefreshTokenUser(UserRefreshTokenDto dto) : IRequest<GenericApiResponse<UserTokensReponse>>;
+
+}
